cef1d0abd85bd1151d410bde9c9bfaa65b5291a8
Emily Ong <emilyohq11@gmail.com>
2020-01-13 21:57:30 +0800

    Edited http links

--- js/weather.js
+++ js/weather.js
@@ -162,8 +162,8 @@ function generateMap (pos) {
   streetMapContainer.appendChild(mapChild);
   // Create street map
   var streetMap = L.map("streetMap").setView([latitude, longitude], 13);
-  L.tileLayer('httpss://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: "Map data &copy; <a href='httpss://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='httpss://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='httpss://www.mapbox.com/'>Mapbox</a>",
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
     maxZoom: 13,
     id: "mapbox/streets-v11",
     accessToken: "pk.eyJ1IjoiZW9ocSIsImEiOiJjazViM2NuNnIxNnFuM2ZvM2ZlMGlkNTRrIn0.rwD8ADA7qb2rckvNqlyw5w"
